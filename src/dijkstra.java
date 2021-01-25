2bc931da7f738bc76b4978c88018158f35dc7df3
Naman Jain <namanjain1568@gmail.com>
2021-01-25 11:24:28 +0530

    Initial Commit

---
+++ src/dijkstra.java
@@ -0,0 +1,6 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class dijkstra {
+    HashMap<Integer, LinkedList<int[]>> graph = new HashMap<Integer, LinkedList<Integer[2]>>();
+}
